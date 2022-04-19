e7c625cbf59c4ac4be3d6166a89753591322c2c0
Bob Fogg <bob.fogg@celltracktech.com>
2022-04-19 15:30:54 -0400

    init build for bullseye ctt username

--- roles/sensorgnome-boot/files/start-sensorgnome.sh
+++ roles/sensorgnome-boot/files/start-sensorgnome.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export SG_PORT=3010 NODE_ENV=production VAMP_PATH=/home/pi/.vamp NODE_PATH=/home/pi/proj/sensorgnome/master/node_modules LC_ALL="C.UTF-8"
-/usr/bin/node /home/pi/proj/sensorgnome/master/master.js
+export SG_PORT=3010 NODE_ENV=production VAMP_PATH=/home/ctt/.vamp NODE_PATH=/home/ctt/proj/sensorgnome/master/node_modules LC_ALL="C.UTF-8"
+/usr/local/bin/node /home/ctt/proj/sensorgnome/master/master.js
