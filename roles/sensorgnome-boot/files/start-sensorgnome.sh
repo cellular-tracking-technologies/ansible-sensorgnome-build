d872d4ae0bc23b1544fe47ccd2f29bb600c759b8
Bob Fogg <bob.fogg@celltracktech.com>
2022-05-25 14:19:12 -0400

    v2,v3 port map; sensorgnome code in /lib/ctt/sensorgnome

--- roles/sensorgnome-boot/files/start-sensorgnome.sh
+++ roles/sensorgnome-boot/files/start-sensorgnome.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export SG_PORT=3010 NODE_ENV=production VAMP_PATH=/home/ctt/.vamp NODE_PATH=/home/ctt/proj/sensorgnome/master/node_modules LC_ALL="C.UTF-8"
-/usr/local/bin/node /home/ctt/proj/sensorgnome/master/master.js
+export SG_PORT=3010 NODE_ENV=production VAMP_PATH=/lib/ctt/sensorgnome/.vamp NODE_PATH=/lib/ctt/senorgnome/sensorgnome/master/node_modules LC_ALL="C.UTF-8"
+/usr/local/bin/node /lib/ctt/sensorgnome/sensorgnome/master/master.js
